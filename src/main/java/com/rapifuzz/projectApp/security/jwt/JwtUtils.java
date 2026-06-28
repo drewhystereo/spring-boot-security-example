a9b44bfce363a496b351b8ce19f8936525be521a
Dhruv Khajotia <dhruv.kdood@gmail.com>
2026-06-28 16:09:51 +0530

    fixed the version

--- src/main/java/com/rapifuzz/projectApp/security/jwt/JwtUtils.java
+++ src/main/java/com/rapifuzz/projectApp/security/jwt/JwtUtils.java
@@ -18,10 +18,10 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${bhanu.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
+  @Value("${bhanu.app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
   public String generateJwtToken(Authentication authentication) {
